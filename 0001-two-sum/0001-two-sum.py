f008ed06d3e6b920d14947af72fd4bd0bf5ffc4b
Yash Vyas <56440961+virushere@users.noreply.github.com>
2024-12-18 17:03:30 -0500

    Time: 0 ms (100%), Space: 19 MB (9.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-        return
+        ## Solution Time complexity O(n^2)
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
+
+        ## Better solution -> 
+        ## Time complexity O(n)
+        prevMap = {} # value : index
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
