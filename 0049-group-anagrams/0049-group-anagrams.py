7580904a9753937799bc74f3b3614cb323f14a8c
Yash Vyas <56440961+virushere@users.noreply.github.com>
2024-12-20 00:12:57 -0500

    Time: 23 ms (20.23%), Space: 22.7 MB (7.69%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # input: strs = ["eat","tea","tan","ate","nat","bat"]
+        # Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+        # Initialize a defaultdict with lists
+        result = defaultdict(list)  
+        
+        # Iterate over each string
+        for s in strs:
+            count = [0] * 26  # Create a frequency count array for characters a-z
+            for c in s:
+                count[ord(c) - ord("a")] += 1  # Update frequency for each character
+            result[tuple(count)].append(s)  # Use the frequency tuple as a key
+            
+        # Convert dict_values to a list
+        return list(result.values())
