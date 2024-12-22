593c68b105c2cc3687543a8ac157bc66a53e5ac5
Yash Vyas <56440961+virushere@users.noreply.github.com>
2024-12-22 00:06:56 -0500

    Time: 7 ms (54.52%), Space: 20.9 MB (34.35%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,15 +4,16 @@ class Solution:
         # Output: [2,3]
         
         ## Solution 1 with time complexity O(nlogn)
-        # frequency_map = defaultdict(int)
-        # for num in nums:
-        #     frequency_map[num] += 1
-        # sorted_list = sorted(frequency_map.keys(), 
-        #                       key=lambda x: frequency_map[x], 
-        #                       reverse=True)
-        # return sorted_list[:k]
+        frequency_map = defaultdict(int)
+        for num in nums:
+            frequency_map[num] += 1
+        sorted_list = sorted(frequency_map.keys(), 
+                        key=lambda x: frequency_map[x], 
+                        reverse=True)
+        return sorted_list[:k]
 
         ## Solution 2 with Time complexity O(n)
+        """ 
         count = {}
         freq = [[] for i in range(len(nums) + 1)]
 
@@ -26,4 +27,5 @@ class Solution:
             for n in freq[i]:
                 res.append(n)
                 if len(res) == k:
-                    return res
+                    return res 
+        """
